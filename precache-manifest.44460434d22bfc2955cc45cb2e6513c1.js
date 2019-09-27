fd30e54cbd3d6f17d7bb59ca7f0b33b723183a36
Stuart Smith <stuart.ingersoll-smith@cherrythinking.com>
2019-09-27 09:59:00 +0100

    Updates

---
+++ precache-manifest.44460434d22bfc2955cc45cb2e6513c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c07e4dd17ac468c1966e2307d5ec63",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "830ba4e1ff2183c04d73",
+    "url": "/react-portfolio/static/css/main.0f4c8bf4.chunk.css"
+  },
+  {
+    "revision": "b7b3fdc7e1cd147f3394",
+    "url": "/react-portfolio/static/js/2.209d1e09.chunk.js"
+  },
+  {
+    "revision": "830ba4e1ff2183c04d73",
+    "url": "/react-portfolio/static/js/main.d54b47c9.chunk.js"
+  },
+  {
+    "revision": "925d611db9c5940fd167",
+    "url": "/react-portfolio/static/js/runtime-main.fd1ceef2.js"
+  }
+]);
